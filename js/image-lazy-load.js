e0b2d6162fe8b62b603137431fda6b49d9f6bb3d
BSTM Ecosystem <myrahmosanako@gmail.com>
2026-03-18 01:52:14 +0200

    Create image-lazy-load.js

---
+++ js/image-lazy-load.js
@@ -0,0 +1,15 @@
+const lazyLoadImages = () => {
+    const images = document.querySelectorAll('img[data-src]');
+    const imageObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src;
+                img.removeAttribute('data-src');
+                imageObserver.unobserve(img);
+            }
+        });
+    });
+    images.forEach(img => imageObserver.observe(img));
+};
+document.addEventListener('DOMContentLoaded', lazyLoadImages);
